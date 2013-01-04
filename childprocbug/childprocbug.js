c0c76411ea73aa5137eeb41b5bfbf33d7dcbf74e
Greg <greg@Gregs-Computer-NCC-1701-A-4.local>
2013-01-04 12:16:43 -0500

    Fixing some sloppy typos and changing spawned process to irb (interactive ruby)

--- childprocbug/childprocbug.js
+++ childprocbug/childprocbug.js
@@ -2,7 +2,8 @@ var Replies = new Meteor.Collection('replies');
 
 if (Meteor.isClient) {
    Template.terminal.window = function() {
-      return replies.findOne().message;
+    if(!Replies.findOne()) return "";
+      return Replies.findOne().message;
     };
 
      Template.terminal.events = {
@@ -14,18 +15,28 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function() {
-    cmd = __meteor_bootstrap__.require('child_process').spawn('cmd');
+    cmd = __meteor_bootstrap__.require('child_process').spawn('irb', [], {detached: true, stdio:'pipe'});
     cmd.stdout.on('data', function(data){
       Fiber(function(){
         Replies.remove({});
         Replies.insert({message: data});
       }).run();
     });
+
+    cmd.stderr.on("data", function(data){
+      console.log("error: " + data);
+    });
+
+    cmd.on('exit', function(){
+      console.log("cmd exited");
+    });
    });
  
  Meteor.methods({
   'command': function(line) {
-    cmd.stdin.write("echo hi \\n");
+    console.log("hi again");
+    cmd.stdin.write('puts "hi" \n');
+    console.log(cmd); 
   }
  });
 }
